ce8bbf637de43ebfe19729af2b557c57f8431183
kaushikreddy1026 <54783830+kaushikreddy1026@users.noreply.github.com>
2020-04-22 22:22:31 -0400

    Update text_sender.py

--- text_sender.py
+++ text_sender.py
@@ -1,10 +1,17 @@
-import datetime; import os; from twilio.rest import Client
-sid = os.environ.get('INSERT_TWILIO_ACCOUNT_SID_HERE')
-token = os.environ.get('INSERT_TWILIO_AUTHETIFICATION_TOKER_HERE')
-client = Client(sid,token)
-now = datetime.datetime.now()
-client.messages.create(from_=os.environ.get('INSERT_TWILIO_PHONE_NUMBER_HERE'),
-                      to=os.environ.get('INSERT_PERSONAL_PHONE_NUMBER_HERE'),
-                      body=("Somebody is speeding at {} on {}, {} {}, {} ".format(now.strftime("%X"),
-                        now.strftime("%A"),now.strftime("%B"),now.strftime("%d"),now.strftime("%Y"))))
-print("Text message successfully sent")
+import datetime;import os;from twilio.rest import Client
+accountsid="INSERT_LIVE_ACCOUNT_SID_HERE"
+accounttoken="INSERT_LIVE_ACCOUNT_TOKEN_HERE"
+twilionumber="INSERT_YOUR_TWILIO_NUMBER_HERE"
+#USE THIS LINK TO FIND YOUR TWILIO ACCOUNT SID, ACCOUNT TOKEN, & PHONE NUMBER -> https://www.twilio.com/console
+mynumber="INSERT_YOUR_PHONE_NUMBER_HERE"
+class ClassTextSend:
+  def FuncTextSend(self):
+    client = Client(accountsid,accounttoken)
+    now = datetime.datetime.now()
+    client.messages.create(from_=(twilionumber),
+                                to=(mynumber),
+                          body=("Somebody is speeding at {} on {}, {} {}, {} ".format(now.strftime("%X"),
+                           now.strftime("%A"),now.strftime("%B"),now.strftime("%d"),now.strftime("%Y"))))
+    print("Text message successfully sent")
+TextSender=ClassTextSend()
+TextSender.FuncTextSend()
