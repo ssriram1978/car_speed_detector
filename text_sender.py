e6c3885bd3c9cf8d39b07a73a46c17d2c8266177
kaushikreddy1026 <54783830+kaushikreddy1026@users.noreply.github.com>
2020-05-08 14:09:51 -0400

    Update text_sender.py

--- text_sender.py
+++ text_sender.py
@@ -1,18 +1,18 @@
-import datetime;import os;from twilio.rest import Client
-accountsid="INSERT_ACCOUNT_SID_HERE"
-accounttoken="INSERT_ACCOUNT_TOKEN_HERE"
-twilionumber="INSERT_TWILIO_PHONE_NUMBERS_HERE"
-#USE THIS LINK TO FIND YOUR TWILIO ACCOUNT SID, ACCOUNT TOKEN, & PHONE NUMBER -> https://www.twilio.com/console
-mynumbers= ["INSERT_ANY_PHONE_NUMBERS_HERE]
-class ClassTextSend:
-  def FuncTextSend(self):
-      for i in range(len(mynumbers)):
-            client = Client(accountsid,accounttoken)
+import datetime; from twilio.rest import Client
+textnumbers = [""]  # INSERT ANY PHONE NUMBERS HERE
+sid = "" # INSERT ACCOUNT SID HERE
+token = "" # INSERT AUTH TOKEN HERE
+twilionumber = "" # INSERT THE TWILIO NUMBER HERE
+client = Client(sid, token)
+def textsend():
+    try:
+        for i in range(len(textnumbers)):
             now = datetime.datetime.now()
             client.messages.create(from_=(twilionumber),
-                                     to=(mynumbers[i]),
-                                 body=("Somebody is speeding at {} on {}, {} {}, {} ".format(now.strftime("%X"),
-                                 now.strftime("%A"),now.strftime("%B"),now.strftime("%d"),now.strftime("%Y"))))
-            print("Text message successfully sent")
-TextSender=ClassTextSend()
-TextSender.FuncTextSend()
+                                    to=(textnumbers[i]),
+                                    body=("Somebody is speeding at {} on {}, {} {}, {} ".format(now.strftime("%X"),
+                                        now.strftime("%A"),now.strftime("%B"), now.strftime("%d"),now.strftime("%Y"))))
+        print(len(textnumbers), "text messages succesfully sent!")
+    except:
+        print("There was an error, and {} messages could not be successfully sent.".format(len(textnumbers)))
+textsend()
