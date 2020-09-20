a594f4b705069a7deba2a4edc58c56ff7b7c7253
Sriram Sridhar <sriramsridharjob78@gmail.com>
2020-09-20 17:14:22 -0400

    Integrated whatsapp message and image send functionality.

--- car_speed_detector/speed_validator.py
+++ car_speed_detector/speed_validator.py
@@ -8,11 +8,12 @@ from car_speed_detector.constants import SEND_EMAIL, MAX_THRESHOLD_SPEED, LOG_FI
 from car_speed_detector.email_sender import EmailSender
 from imutils.io import TempFile
 from car_speed_detector.whats_app_message_sender import WhatsAppMessageSender
+from car_speed_detector.car_speed_logging import logger
 
 
 class SpeedValidator:
     log_file = None
-    
+
     @classmethod
     def close_log_file(cls):
         # check if the log file object exists, if it does, then close it
@@ -22,7 +23,7 @@ class SpeedValidator:
     @classmethod
     def initialize_log_file(cls):
         if not cls.log_file:
-            cls.log_file = open(os.path.join(Path(__file__).parent, LOG_FILE_NAME),mode="a")
+            cls.log_file = open(os.path.join(Path(__file__).parent, LOG_FILE_NAME), mode="a")
         # set the file pointer to end of the file
         if cls.log_file.seek(0, os.SEEK_END) == 0:
             cls.log_file.write("Year,Month,Day,Time (in MPH),Speed\n")
@@ -39,7 +40,7 @@ class SpeedValidator:
             # check if the object's speed has been estimated and it
             # is higher than the speed limit
             if trackable_object.estimated and trackable_object.speedMPH > MAX_THRESHOLD_SPEED:
-                mid_point = len(trackable_object.tracked_object_frame_list)//2
+                mid_point = len(trackable_object.tracked_object_frame_list) // 2
                 time_stamp = trackable_object.timestamp_list[mid_point]
                 frame = trackable_object.tracked_object_frame_list[mid_point]
 
@@ -54,22 +55,26 @@ class SpeedValidator:
                     imageID = time_stamp.strftime("%H%M%S%f")
                     tempFile = TempFile()
 
-                    #write the date and speed on the image.
+                    # write the date and speed on the image.
                     cv2.putText(frame, datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-                            (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 1)
+                                (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 1)
                     # write the speed: first get the size of the text
-                    size, base = cv2.getTextSize( "%.0f mph" % trackable_object.speedMPH, cv2.FONT_HERSHEY_SIMPLEX, 2, 3)
+                    size, base = cv2.getTextSize("%.0f mph" % trackable_object.speedMPH, cv2.FONT_HERSHEY_SIMPLEX, 2, 3)
                     # then center it horizontally on the image
-                    cntr_x = int((frame.shape[1] - size[0]) / 2) 
+                    cntr_x = int((frame.shape[1] - size[0]) / 2)
                     cv2.putText(frame, "%.0f mph" % trackable_object.speedMPH,
-                            (cntr_x , int(frame.shape[0] * 0.2)), cv2.FONT_HERSHEY_SIMPLEX, 2.00, (0, 255, 0), 3)
+                                (cntr_x, int(frame.shape[0] * 0.2)), cv2.FONT_HERSHEY_SIMPLEX, 2.00, (0, 255, 0), 3)
                     cv2.imwrite(tempFile.path, frame)
 
                     # create a thread to send the image via email.
                     # and start it
-                    t = Thread(target=EmailSender.send_email, args=(tempFile,imageID,))
+                    t = Thread(target=EmailSender.send_email, args=(tempFile, imageID,))
                     t.start()
-                    t2 = Thread(target=WhatsAppMessageSender().send_message(speed=trackable_object.speedMPH, temp_file=tempFile, image_name=imageID))
+                    image_path = os.path.join(os.getcwd(), "{}.jpg".format(imageID))
+                    logger().info("Writing car image {} to hard drive.".format(image_path))
+                    cv2.imwrite(image_path, frame)
+                    t2 = Thread(target=WhatsAppMessageSender().send_message(speed=trackable_object.speedMPH,
+                                                                            image_path=image_path))
                     t2.start()
                     # log the event in the log file
                     info = "{},{},{},{},{},{}\n".format(year, month,
--- car_speed_detector/unit_tests/test_whatsapp_message_sender.py
+++ car_speed_detector/unit_tests/test_whatsapp_message_sender.py
@@ -1,5 +1,5 @@
 # This file is used to test the car speed whatsapp message sender functionality.
-
+import os
 import unittest
 from car_speed_detector.whats_app_message_sender import WhatsAppMessageSender
 
@@ -13,4 +13,5 @@ class TestWhatsappMessageSender(unittest.TestCase):
         whatsapp_message_sender_inst = WhatsAppMessageSender(
             browser_location='/usr/bin/google-chrome',
             browser_executable_path='/home/sriramsridhar/Downloads/chromedriver')
-        self.assertEqual(whatsapp_message_sender_inst.send_message(20, None, None), True)
+        image_path = os.path.join(os.getcwd(), 'car_test.jpg')
+        self.assertEqual(whatsapp_message_sender_inst.send_message(20, image_path=image_path), None)
