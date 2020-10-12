0f88b35fbf7063e5710e473ddd66c76e6ca6d610
Sriram Sridhar <sriramsridharjob78@gmail.com>
2020-10-12 19:29:46 -0400

    Enhanced on_reboot script to log the messages to a file.

--- on_reboot.sh
+++ on_reboot.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-echo "source /usr/local/bin/virtualenvwrapper.sh" >> /home/pi/test.log
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> /home/pi/car_speed_detector.log
 source /usr/local/bin/virtualenvwrapper.sh
-echo "source /opt/intel/openvino/bin/setupvar.sh" >> /home/pi/test.log
+echo "source /opt/intel/openvino/bin/setupvar.sh" >> /home/pi/car_speed_detector.log
 source /opt/intel/openvino/bin/setupvars.sh
-echo "loading virtualenvwrapper.sh..." >> /home/pi/test.log
+echo "loading virtualenvwrapper.sh..." >> /home/pi/car_speed_detector.log
 source `which virtualenvwrapper.sh`
-echo "accessing virtualenv..." >> /home/pi/test.log
+echo "accessing virtualenv..." >> /home/pi/car_speed_detector.log
 workon py3cv4
 cd /home/pi
 echo "export PYTHONPATH=$PYTHONPATH:/home/pi/git/car_speed_detector"
 export PYTHONPATH=$PYTHONPATH:/home/pi/git/car_speed_detector
-echo "running Python script..." >> /home/pi/test.log
-python3 /home/pi/git/car_speed_detector/car_speed_detector/speed_detector.py 
-echo "script exiting..." >> /home/pi/test.log
+echo "running Python script..." >> /home/pi/car_speed_detector.log
+python3 /home/pi/git/car_speed_detector/car_speed_detector/speed_detector.py >> /home/pi/car_speed_detector.log
+echo "script exiting..." >> /home/pi/car_speed_detector.log
