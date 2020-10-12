08fe20eaf8567ec8647ded5503274a5b5b7efb30
Sriram Sridhar <sriramsridharjob78@gmail.com>
2020-10-12 15:44:01 -0400

    Reverting the openvino toolkit version to 2020.1 because there is an issue with 2020.4 version.

--- install_openvino.sh
+++ install_openvino.sh
@@ -3,8 +3,8 @@ echo "cd ~/Downloads/"
 cd ~/Downloads/
 echo "sudo mkdir -p /opt/intel/openvino"
 sudo mkdir -p /opt/intel/openvino
-echo "wget https://download.01.org/opencv/2020/openvinotoolkit/2020.4/l_openvino_toolkit_runtime_raspbian_p_2020.4.287.tgz"
-wget https://download.01.org/opencv/2020/openvinotoolkit/2020.4/l_openvino_toolkit_runtime_raspbian_p_2020.4.287.tgz
+echo "wget https://download.01.org/opencv/2020/openvinotoolkit/2020.1/l_openvino_toolkit_runtime_raspbian_p_2020.1.023.tgz"
+wget https://download.01.org/opencv/2020/openvinotoolkit/2020.1/l_openvino_toolkit_runtime_raspbian_p_2020.1.023.tgz
 echo "sudo tar -xf  l_openvino_toolkit_runtime_raspbian_p_*.tgz --strip 1 -C /opt/intel/openvino"
 sudo tar -xf  l_openvino_toolkit_runtime_raspbian_p_*.tgz --strip 1 -C /opt/intel/openvino
 echo "source /opt/intel/openvino/bin/setupvars.sh"
