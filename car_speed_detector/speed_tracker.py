Fixed the algorithm for computing speed based on p50 (50th percentile) instead of computing average of all detections. Also, modified the logic to discard values less than 5 miles per hour.

car_speed_detector/speed_tracker.py
@@ -1,7 +1,8 @@
 # This class servers as a common place to store all the centroids, timestamp, position and speed computed for a
 # identified object.
 import numpy as np
-from car_speed_detector.constants import MILES_PER_ONE_KILOMETER, SPIKE_THRESHOLD
+from car_speed_detector.constants import DISCARD_SPEED_VALUE, MAX_NUM_OF_CONSECUTIVE_FRAMES_FOR_ACTION, \
+    MILES_PER_ONE_KILOMETER, SPIKE_THRESHOLD
 from car_speed_detector.car_speed_logging import logger
 #import pandas as pd
 
@@ -49,34 +50,9 @@ class SpeedTracker:
         """
         # calculate the speed in MPH
         self.speedMPH_list = list(map(lambda x: x*MILES_PER_ONE_KILOMETER, self.estimated_speed_list))
-        logger().info("self.speedMPH_list = {}".format(self.speedMPH_list))
-        if len(self.speedMPH_list) == 0:
+        if len(self.speedMPH_list) < MAX_NUM_OF_CONSECUTIVE_FRAMES_FOR_ACTION:
             return
-        elif len(self.speedMPH_list) == 1:
-            if self.speedMPH_list[0] < SPIKE_THRESHOLD:
-                self.speedMPH = self.speedMPH_list[0]
-            return
-        
-        # Filter out skewed up values. [5, 10, 8, 100, 8, 3] => [5, 10, 8, 8, 3] 
-        # self.speedMPH_list = [2.3114675882339433, 9.484122199976445, 2.376011471462965, 6.813924186015028, 72.87371060928051]
-        # Filter out values which are greater than its predecessor by a large value. Example: If the speedMPH_list = [1,2,1,100,2] remove 100 from this list bec ause 100 is skewed up than its predecessor by a large spike.
-        temp_speedMPH_list = []
-        for index in range(len(self.speedMPH_list)):
-            if index == 0 and abs(self.speedMPH_list[index] - self.speedMPH_list[index+1]) < SPIKE_THRESHOLD:
-                temp_speedMPH_list.append(self.speedMPH_list[index])
-            elif index == len(self.speedMPH_list)-1:
-                diff_between_last_and_last_but_one = abs(self.speedMPH_list[index] - self.speedMPH_list[index-1]) 
-                if diff_between_last_and_last_but_one < SPIKE_THRESHOLD :
-                    temp_speedMPH_list.append(self.speedMPH_list[index])
-                else:
-                    continue
-            elif abs(self.speedMPH_list[index]-self.speedMPH_list[index+1]) > SPIKE_THRESHOLD and \
-            abs(self.speedMPH_list[index] - self.speedMPH_list[index-1]) > SPIKE_THRESHOLD:
-                    continue
-            else:
-                temp_speedMPH_list.append(self.speedMPH_list[index])
-        self.speedMPH_list = temp_speedMPH_list
-        logger().info("spike corrected self.speedMPH_list = {}".format(self.speedMPH_list))
-        self.speedMPH = np.average(self.speedMPH_list)
-        #self.Speeds = self.Speeds.append(self.speedMPH, ignore_index=True)
-        #self.Speeds.to_csv('Speeds.csv')
+        sorted_mph_list = [ x for x in sorted(self.speedMPH_list) if x > DISCARD_SPEED_VALUE]
+        logger().info("self.object_id = {}, sorted_mph_list = {}".format(self.object_id, sorted_mph_list))
+        if len(sorted_mph_list):
+            self.speedMPH = np.percentile(sorted_mph_list, 50)